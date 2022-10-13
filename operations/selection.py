e33ae8e527b4d0b4c74f418d3a2a6dfe2e494628
mramin22 <dadgaramin96@gmail.com>
2022-10-13 18:18:45 +0330

    fitnesses are now assumed to be fed into selection functions!

--- operations/selection.py
+++ operations/selection.py
@@ -1,9 +1,9 @@
-from numpy import random
+from numpy import random, where
 """
 Selection methods 
 """
 
-def roulette_wheel(population, selection_pop_size, fitness_function, with_replacement = True) -> list:
+def roulette_wheel(population, selection_pop_size, pop_fitness, with_replacement = True) -> list:
     """
     roulette wheel (fitness proportionate) Method to select the new population
     
@@ -13,9 +13,9 @@ def roulette_wheel(population, selection_pop_size, fitness_function, with_replac
     population : array
         array of genes
         each gene corresponds to one person
-    fitness_function : function
-        the function that calculates the fitness
-        note that the input for the function should be a string gene (one string input)
+    pop_fitness : array
+        the fitness of population
+        each item of array indicates each gene fitness
     selction_pop_size : int
         specify how many genes to return
     with_replacement : bool
@@ -29,16 +29,9 @@ def roulette_wheel(population, selection_pop_size, fitness_function, with_replac
     """
 
     # find the probability of selection of each gene in population
+    population_probability = pop_fitness / sum(pop_fitness)
 
-    ## intialize the population fitness array
-    fitness_pop = []
-    for gene in population:
-        fitness_gene = fitness_function(gene)
-
-        fitness_pop.append(fitness_gene)
-
-    population_probability = fitness_pop / sum(fitness_pop)
-
+    # new population selection
     selected_population = random.choice(population, 
                             size=selection_pop_size,
                             p=population_probability,
@@ -47,7 +40,7 @@ def roulette_wheel(population, selection_pop_size, fitness_function, with_replac
     return selected_population
 
 
-def binary_tournament(population, selection_pop_size, fitness_function, with_replacement = True) -> list:
+def binary_tournament(population, selection_pop_size, pop_fitness) -> list:
     """
     binary tournament Method to select the new population
     
@@ -56,30 +49,31 @@ def binary_tournament(population, selection_pop_size, fitness_function, with_rep
     population : array
         array of genes
         each gene corresponds to one person
-    fitness_function : function
-        the function that calculates the fitness
-        note that the input for the function should be a string gene (one string input)
+    pop_fitness : array
+        the fitness of population
+        each item of array indicates each gene fitness
     selction_pop_size : int
         specify how many genes to return
-    with_replacement : bool
-        select genes from population with replacement or not
-        default is True, meaning a gene can be chosen multiple times 
     
     Output:
     --------
     selected_population : array
         the selected population for the new generation
     """
-    ## initialize an empty array
+    # initialize an empty array
     selected_population = []
 
-    ## the gene selection
+    # the gene selection
     for _ in range(selection_pop_size):
-        ## get out two gene from population randomly (uniform random)
+        # get out two gene from population randomly (uniform random)
         gene1, gene2 = random.choice(population, size=2)
+        
+        ## find the indexes in order to get the fitness from pop_fitness array
+        gene1_idx = where(population == gene1)
+        gene2_idx = where(population == gene2)
 
-        ## evaluate which has a better fitness
-        if fitness_function(gene1) > fitness_function(gene2):
+        # evaluate which has a better fitness
+        if pop_fitness[gene1_idx] > pop_fitness[gene2_idx]:
             selected_population.append(gene1)
         else:
             selected_population.append(gene2)

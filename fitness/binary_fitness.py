52c592c9e04f160692fb0550ba92a603bb93f061
mramin22 <dadgaramin96@gmail.com>
2022-10-13 15:58:23 +0330

    binary fitness functions are now without class there was no special need to have a class of binary fitness functions

--- fitness/binary_fitness.py
+++ fitness/binary_fitness.py
@@ -1,278 +1,270 @@
-import numpy as np
+"fitness functions for binary genes in evolutionary algorithm"
 
+def onemax(string_gene) -> int:
+    """
+    OneMax fitness function
+    size of chromosomes with value `1`
 
-class binary_fitness():
-    "Binary fitness functions for evolutionary algorithm"
+    Parameters:
+    ------------
+    string_gene : string
+        a string of zeros and ones representing chromosomes of a gene
 
+    Output:
+    ---------
+    fitness : int
+        the fitness of the gene
+    """
+    fitness = str.count(string_gene, '1')
+    return fitness
 
-    def __init__(self) -> None:
-        pass
+def peak(string_gene) -> int:
+    """
+    peak fitness function
+    multiplication of the chromosome
 
-    def onemax(self, string_gene) -> int:
-        """
-        OneMax fitness function
-        size of chromosomes with value `1`
+    Parameters:
+    ------------
+    string_gene : string
+        a string of zeros and ones representing chromosomes of a gene
 
-        Parameters:
-        ------------
-        string_gene : string
-            a string of zeros and ones representing chromosomes of a gene
+    Output:
+    ---------
+    fitness : int
+        the fitness of the gene
+    """
 
-        Output:
-        ---------
-        fitness : int
-            the fitness of the gene
-        """
-        fitness = str.count(string_gene, '1')
-        return fitness
+    fitness = None
+    ## if a zero was available in string, then fitness will be zero
+    ## else 1
+    if str.count(string_gene, '0') > 0:
+        fitness = 0
+    else:
+        fitness = 1
+    
+    ## if any problems happen 
+    if fitness == None:
+        raise f"Error! fitness in peak function is not set!\nCheck your gene: {string_gene}" 
 
-    def peak(self, string_gene) -> int:
-        """
-        peak fitness function
-        multiplication of the chromosome
-
-        Parameters:
-        ------------
-        string_gene : string
-            a string of zeros and ones representing chromosomes of a gene
-
-        Output:
-        ---------
-        fitness : int
-            the fitness of the gene
-        """
-
-        fitness = None
-        ## if a zero was available in string, then fitness will be zero
-        ## else 1
-        if str.count(string_gene, '0') > 0:
-            fitness = 0
-        else:
-            fitness = 1
-        
-        ## if any problems happen 
-        if fitness == None:
-            raise f"Error! fitness in peak function is not set!\nCheck your gene: {string_gene}" 
-
-        
-        return fitness
+    
+    return fitness
 
     
 
-    def flipflop(self, string_gene) -> int:
-        """
-        flipflop fitness function
-        xor the chromosomes with each other
+def flipflop(string_gene) -> int:
+    """
+    flipflop fitness function
+    xor the chromosomes with each other
 
-        Parameters:
-        ------------
-        string_gene : string
-            a string of zeros and ones representing chromosomes of a gene
+    Parameters:
+    ------------
+    string_gene : string
+        a string of zeros and ones representing chromosomes of a gene
 
-        Output:
-        ---------
-        fitness : int
-            the fitness of the gene
-        """
+    Output:
+    ---------
+    fitness : int
+        the fitness of the gene
+    """
 
-        ## to avoid loop we will shift the binary gene and xor it with the shifted one
+    ## to avoid loop we will shift the binary gene and xor it with the shifted one
 
-        ## shifting and adding the first chromosome of the gene to have the same length
-        ## same as logical right shift
-        string_gene_shifted = string_gene[0] + string_gene[:-1]
+    ## shifting and adding the first chromosome of the gene to have the same length
+    ## same as logical right shift
+    string_gene_shifted = string_gene[0] + string_gene[:-1]
 
-        ## xor operation can now be applied
-        genes_xor = bin(int(string_gene, 2) ^ int(string_gene_shifted, 2))[2:]
+    ## xor operation can now be applied
+    genes_xor = bin(int(string_gene, 2) ^ int(string_gene_shifted, 2))[2:]
 
-        ## count of ones in gene is the fitness
-        fitness = str.count(genes_xor, '1')
+    ## count of ones in gene is the fitness
+    fitness = str.count(genes_xor, '1')
 
-        return fitness
+    return fitness
 
 
-    def fourpeaks(self, string_gene, T) -> int:
-        """
-        fourpeaks fitness function
-        equation is
-        `max(tail(0, string_gene), head(1, string_gene)) + R`
-        where `R` is `gene_length` if both `tail(0, string_gene)` and `head(1, string_gene)` are bigger than an integer `T` else is `0` 
-        `tail` and `head` are functions that count the zeros in the tail and ones in the head of the gene respectively 
+def fourpeaks(string_gene, T) -> int:
+    """
+    fourpeaks fitness function
+    equation is
+    `max(tail(0, string_gene), head(1, string_gene)) + R`
+    where `R` is `gene_length` if both `tail(0, string_gene)` and `head(1, string_gene)` are bigger than an integer `T` else is `0` 
+    `tail` and `head` are functions that count the zeros in the tail and ones in the head of the gene respectively 
 
-        Parameters:
-        ------------
-        string_gene : string
-            a string of zeros and ones representing chromosomes of a gene
-        T : integer
-            hyperparameter for the `R` function
+    Parameters:
+    ------------
+    string_gene : string
+        a string of zeros and ones representing chromosomes of a gene
+    T : integer
+        hyperparameter for the `R` function
 
-        Output:
-        ---------
-        fitness : int
-            the fitness of the gene
-        """
+    Output:
+    ---------
+    fitness : int
+        the fitness of the gene
+    """
 
-        fitness = max(self.__tail(string_gene, '0'), self.__head(string_gene, '1')) + self.__R(string_gene, T, 'fourpeak')
+    fitness = max(tail(string_gene, '0'), head(string_gene, '1')) + R(string_gene, T, 'fourpeak')
 
-        return fitness
+    return fitness
 
-    def sixpeaks(self, string_gene, T) -> int:
-        """
-        sixpeaks fitness function
-        equation is
-        `max(tail(0, string_gene), head(1, string_gene)) + R`
-        where `R` is `gene_length` if both `tail(0 or 1, string_gene)` and `head(1 or 0, string_gene)` are bigger than an integer `T` else is `0` 
-        `tail` and `head` are functions that count the zeros in the tail and ones in the head of the gene respectively 
+def sixpeaks(string_gene, T) -> int:
+    """
+    sixpeaks fitness function
+    equation is
+    `max(tail(0, string_gene), head(1, string_gene)) + R`
+    where `R` is `gene_length` if both `tail(0 or 1, string_gene)` and `head(1 or 0, string_gene)` are bigger than an integer `T` else is `0` 
+    `tail` and `head` are functions that count the zeros in the tail and ones in the head of the gene respectively 
 
-        Parameters:
-        ------------
-        string_gene : string
-            a string of zeros and ones representing chromosomes of a gene
-        T : integer
-            hyperparameter for the `R` function
+    Parameters:
+    ------------
+    string_gene : string
+        a string of zeros and ones representing chromosomes of a gene
+    T : integer
+        hyperparameter for the `R` function
 
-        Output:
-        ---------
-        fitness : int
-            the fitness of the gene
-        """
+    Output:
+    ---------
+    fitness : int
+        the fitness of the gene
+    """
 
-        fitness = max(self.__tail(string_gene, '0'), self.__head(string_gene, '1')) + self.__R(string_gene, T, 'sixpeak')
+    fitness = max(tail(string_gene, '0'), head(string_gene, '1')) + R(string_gene, T, 'sixpeak')
 
-        return fitness
+    return fitness
 
-    def trap(self, string_gene):
-        """
-        Trap fitness function
-        equation is
-        `3 * gene_length * peak(string_gene) - onemax(string_gene)` 
+def trap(string_gene):
+    """
+    Trap fitness function
+    equation is
+    `3 * gene_length * peak(string_gene) - onemax(string_gene)` 
+    
+    Parameters:
+    ------------
+    string_gene : string
+        a string of zeros and ones representing chromosomes of a gene
         
-        Parameters:
-        ------------
-        string_gene : string
-            a string of zeros and ones representing chromosomes of a gene
-            
-        Output:
-        ---------
-        fitness : int
-            the fitness of the gene
-        """
-        fitness = 3 * len(string_gene) * self.peak(string_gene) - self.onemax(string_gene)
+    Output:
+    ---------
+    fitness : int
+        the fitness of the gene
+    """
+    fitness = 3 * len(string_gene) * peak(string_gene) - onemax(string_gene)
 
-        return fitness
+    return fitness
 
 
-    def __R(self, string_gene, T, fitness_function):
-        """
-        find the value for the `R` function in for either sixpeak or fourpeak fitness functions
+def R(string_gene, T, fitness_function):
+    """
+    find the value for the `R` function in for either sixpeak or fourpeak fitness functions
 
-        Parameters:
-        ------------
-        string_gene : string
-            a string of zeros and ones representing chromosomes of a gene
-        T : integer
-            hyperparameter for the `R` function
-        fitness_function : string
-            is either 'sixpeak' or 'fourpeak'
+    Parameters:
+    ------------
+    string_gene : string
+        a string of zeros and ones representing chromosomes of a gene
+    T : integer
+        hyperparameter for the `R` function
+    fitness_function : string
+        is either 'sixpeak' or 'fourpeak'
 
-        Output:
-        ---------
-        R_res : int
-            the result function
-        """
+    Output:
+    ---------
+    R_res : int
+        the result function
+    """
 
-        if fitness_function == 'sixpeak':
-            ## because the conditions were too lengthy, we seperated them in two variables
-            condition1 = (self.__tail(string_gene, '0') > T) or self.__head(string_gene, '1') > T
-            condition2 = (self.__tail(string_gene, '1') > T) or self.__head(string_gene, '0') > T
+    if fitness_function == 'sixpeak':
+        ## because the conditions were too lengthy, we seperated them in two variables
+        condition1 = (tail(string_gene, '0') > T) or head(string_gene, '1') > T
+        condition2 = (tail(string_gene, '1') > T) or head(string_gene, '0') > T
 
-            condition = condition1 or condition2
+        condition = condition1 or condition2
 
-        elif fitness_function == 'fourpeak':
-            condition = (self.__tail(string_gene, '0') > T) or self.__head(string_gene, '1') > T
-        else:
-            raise ValueError(f'fitness_function variable should be either \'sixpeak\' or \'fourpeak\'\nNow is {fitness_function}')
+    elif fitness_function == 'fourpeak':
+        condition = (tail(string_gene, '0') > T) or head(string_gene, '1') > T
+    else:
+        raise ValueError(f'fitness_function variable should be either \'sixpeak\' or \'fourpeak\'\nNow is {fitness_function}')
 
 
-        if condition:
-            R_res = len(string_gene)
-        else:
-            R_res = 0
+    if condition:
+        R_res = len(string_gene)
+    else:
+        R_res = 0
 
-        return R_res
+    return R_res
         
 
 
-    def __tail(self, string_gene, value):
-        """
-        find count of values with `value` in gene in the tail of it
+def tail(string_gene, value):
+    """
+    find count of values with `value` in gene in the tail of it
 
-        Parameters:
-        ------------
-        string_gene : string
-            a string of zeros and ones representing chromosomes of a gene
-        T : integer
-            value of chromosome
-            shoud be either `0` or `1` else raise an error
+    Parameters:
+    ------------
+    string_gene : string
+        a string of zeros and ones representing chromosomes of a gene
+    T : integer
+        value of chromosome
+        shoud be either `0` or `1` else raise an error
 
-        Output:
-        ---------
-        count : int
-            the count of tailing values in gene  
-        """
+    Output:
+    ---------
+    count : int
+        the count of tailing values in gene  
+    """
 
-        ## to find value counts in tail
-        ## reverse it and find values in the head
-        reversed_string_gene = string_gene[::-1]
+    ## to find value counts in tail
+    ## reverse it and find values in the head
+    reversed_string_gene = string_gene[::-1]
 
-        count = self.__head(reversed_string_gene, value)
+    count = head(reversed_string_gene, value)
 
-        return count
-
-
-    def __head(self, string_gene, value):
-        """
-        find count of values with `value` in gene in the head of it
-
-        Parameters:
-        ------------
-        string_gene : string
-            a string of zeros and ones representing chromosomes of a gene
-        value : integer
-            value of chromosome
-            shoud be either `0` or `1` else raise an error
-        
-        Output:
-        ---------
-        count : int
-            the count of heading values in gene  
-        """
-        
-        ## check if the value is right
-        self.__check_chromosome(value)
-
-        ## to find the count of chromosomes with value `1` we can count the zeros in the head of gene
-        not_value = '0' if value == '1' else '1'
-
-        ## index of the value, can be also assumed as count
-        count = string_gene.find(not_value)
-
-        ## if not any chromosome with `value` was found!
-        if count == -1:
-            count = 0
-
-        return count
+    return count
 
 
-    def __check_chromosome(self, chromosome):
-        """
-        check the chromosome be either zero or one
+def head(string_gene, value):
+    """
+    find count of values with `value` in gene in the head of it
 
-        Parameters:
-        ------------
-        chromosome : binary 0 or 1 string
-        """
-        if not(chromosome == '0' or chromosome == '1'):
-            print(chromosome, type(chromosome))
-            raise ValueError(f"Chromosome in the gene should be either 0 or 1!\n chromosome is now {chromosome}")
+    Parameters:
+    ------------
+    string_gene : string
+        a string of zeros and ones representing chromosomes of a gene
+    value : integer
+        value of chromosome
+        shoud be either `0` or `1` else raise an error
+    
+    Output:
+    ---------
+    count : int
+        the count of heading values in gene  
+    """
+    
+    ## check if the value is right
+    check_chromosome(value)
+
+    ## to find the count of chromosomes with value `1` we can count the zeros in the head of gene
+    not_value = '0' if value == '1' else '1'
+
+    ## index of the value, can be also assumed as count
+    count = string_gene.find(not_value)
+
+    ## if not any chromosome with `value` was found!
+    if count == -1:
+        count = 0
+
+    return count
+
+
+def check_chromosome(chromosome):
+    """
+    check the chromosome be either zero or one
+
+    Parameters:
+    ------------
+    chromosome : binary 0 or 1 string
+    """
+    if not(chromosome == '0' or chromosome == '1'):
+        print(chromosome, type(chromosome))
+        raise ValueError(f"Chromosome in the gene should be either 0 or 1!\n chromosome is now {chromosome}")
 
         

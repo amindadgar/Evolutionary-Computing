c3760d85d54992d00824de0cf94a4e60cbe38e53
mramin22 <dadgaramin96@gmail.com>
2022-12-02 13:51:23 +0330

    Saving the current work! The model has been created and training functions are implemented, but not tested! And after that the fitness function must be written based on the training of the functions.

--- HW3/fitness.py
+++ HW3/fitness.py
@@ -1,3 +1,4 @@
+from util import convert_genotype_to_phenotype_values
 
 def static_fitness(chromosome):
     """
@@ -5,3 +6,25 @@ def static_fitness(chromosome):
     """
 
     return 1
+
+def fitness_training(chromosome, training_count = 5):
+    """
+    find the fitness of the chromosome with training the model and returning the `training_count` average test accuracies
+
+    Parameters:
+    ------------
+    chromosome : string
+        the string representing the architecture of the transformer network
+    training_count : int
+        the count of training needs for averaging the fitness value
+    
+    Returns:
+    ---------
+    chromsome_fitness : float
+        the average accuracy of test set in 5 time training the network
+    """
+
+    phenotype_values = convert_genotype_to_phenotype_values(chromosome)
+    ## TODO
+
+    
